749c8f6a0ea3b51ee43780ef03083a6d7e8e6635
root <root@DESKTOP-GVQ0Q5H.localdomain>
2022-11-18 14:29:45 +0100

    ss

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,26 @@
-import React from "react";
-import { Meme } from "../Meme/Meme";
-import { Routes, Route } from "react-router-dom";
-import { MemeGenerated } from "../MemeGenerated/MemeGenerated";
+import React from 'react'
+import { Meme } from '../Meme/Meme'
+import { Routes, Route } from 'react-router-dom'
+import { MemeGenerated } from '../MemeGenerated/MemeGenerated'
+import styles from './styles.module.css'
 
 export const App = () => {
   return (
-    <div>
-      <h1>Random Meme Generator</h1>
+    <div className={styles.maincontainer}>
+      <h1 className=''>Random Meme Generator</h1>
       <Routes>
-        <Route exact path="/" element={<Meme />} />
-        <Route path="/generated" element={<MemeGenerated />} />
+        <Route exact path='/' element={<Meme />} />
+        <Route path='/generated' element={<MemeGenerated />} />
       </Routes>
+
+      <a
+        className={styles.link}
+        href='https://raffaelli.studio'
+        target='_blank'
+        rel='noreferrer'
+      >
+        raffaelli.studio
+      </a>
     </div>
-  );
-};
+  )
+}
